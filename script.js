95759025bd9013f20675266523b040e197f0f4b9
Welton Santos <110510225+weltonsj@users.noreply.github.com>
2022-08-03 13:09:17 -0300

    Atualização de versão

--- script.js
+++ script.js
@@ -28,3 +28,54 @@ function carregar() {
         document.body.style.background = '#3f355a'
     }
 }
+
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Cirança
+                img.setAttribute('src', './imagem/bebehomem.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', './imagem/jovemhomem.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', './imagem/adultohomem.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', './imagem/idosohomem.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Cirança
+                img.setAttribute('src', './imagem/bebemulher.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', './imagem/jovemmulher.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', './imagem/adultomulher.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', './imagem/idosomulher.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
